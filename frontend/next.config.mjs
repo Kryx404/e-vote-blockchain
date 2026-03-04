8afc8c79821e42b07c234da58f2786fbe1ba62d6
rifkisyhd <rifkisyahada81@gmail.com>
2026-03-04 23:57:13 +0700

    UPDATE: refactor backend URL configuration in next.config.mjs for improved flexibility

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
+const backendURL =
+    process.env.NEXT_PUBLIC_BACKEND_URL || "http://evote-backend:8080";
+
 const nextConfig = {
     output: "standalone",
     async rewrites() {
         return [
             {
                 source: "/api/v1/:path*",
-                destination: "http://evote-backend:8080/api/v1/:path*",
-                // destination: "http://localhost:8080/api/v1/:path*",
+                destination: `${backendURL}/api/v1/:path*`,
             },
         ];
     },
